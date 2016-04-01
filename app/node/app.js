7c74aac9fe26507cec79873e470d8d452e9a3dee
Dale Fenton <fentondale@gmail.com>
2016-04-01 10:58:01 -0400

    changing server setup

--- app/node/app.js
+++ app/node/app.js
@@ -7,11 +7,14 @@ app.use(cors()); //allows overriding cross origin policy (use npm install if nee
 
 
 app.get('/api', function(req, res){ // listens for request on /api route
-  var lat = req.query.lat; // grabs lat and lng queries from the request object
-  var lng = req.query.lng;
-  request('https://api.brewerydb.com/v2/search/geo/point?lat=' + lat + '&lng=' + lng + '&type=beer&hasImages=Y&key=72a751214ab8b53056ac0a6d8376dc2d', function (error, response, body) { // api url
+  // var lat = req.query.lat; // grabs lat and lng queries from the request object
+  // var lng = req.query.lng;
+  var code = req.query.token;
+  var client_id = process.env.CLIENT_ID;
+  var client_secret = process.env.CLIENT_SECRET;
+  request('https://github.com/login/oauth/access_token?client_id=' + client_id + '&client_secret=' + client_secret + '&code=' + code + '&hasImages=Y&key=72a751214ab8b53056ac0a6d8376dc2d', function (error, response, body) { // api url
     if (!error && response.statusCode === 200) {
-      console.log('beer');
+      console.log('');
       res.send(body); // if no errors, send the body of data back to front end
     }
    });
@@ -20,6 +23,3 @@ app.get('/api', function(req, res){ // listens for request on /api route
 var port = process.env.PORT || 3000;
 app.listen(port);
 console.log('Server running on port %d');
-
-
-// 'https://api.brewerydb.com/v2/search/geo/point?lat=' + lat + '&lng=' + lng + '&type=beer&hasLabel=Y&key=72a751214ab8b53056ac0a6d8376dc2d'
