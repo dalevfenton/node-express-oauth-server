66c6bff4321216355914fea3080ad87efad97940
Dale Fenton <fentondale@gmail.com>
2016-04-01 11:06:46 -0400

    setting up login code request

--- app/node/app.js
+++ app/node/app.js
@@ -7,19 +7,34 @@ app.use(cors()); //allows overriding cross origin policy (use npm install if nee
 
 
 app.get('/api', function(req, res){ // listens for request on /api route
-  // var lat = req.query.lat; // grabs lat and lng queries from the request object
-  // var lng = req.query.lng;
   var code = req.query.token;
   var client_id = process.env.CLIENT_ID;
   var client_secret = process.env.CLIENT_SECRET;
-  request('https://github.com/login/oauth/access_token?client_id=' + client_id + '&client_secret=' + client_secret + '&code=' + code + '&hasImages=Y&key=72a751214ab8b53056ac0a6d8376dc2d', function (error, response, body) { // api url
-    if (!error && response.statusCode === 200) {
-      console.log('');
-      res.send(body); // if no errors, send the body of data back to front end
-    }
+  request('https://github.com/login/oauth/access_token?client_id=' + client_id
+            + '&client_secret=' + client_secret
+            + '&code=' + code,
+    function (error, response, body) { // api url
+      if (!error && response.statusCode === 200) {
+        console.log('getting auth_token');
+        console.log(response);
+        console.log(body);
+        res.send(body); // if no errors, send the body of data back to front end
+      }
    });
 });
 
+app.get('/login', function(req, res){
+  var clientId = process.env.CLIENT_ID;
+  request('https://github.com/login/oauth/authorize?client_id='+clientID+'&scope=repo',
+    function (error, response, body) { // api url
+      if (!error && response.statusCode === 200) {
+        console.log('getting initial code');
+        console.log(response);
+        console.log(body);
+        res.send(body); // if no errors, send the body of data back to front end
+      }
+   });
+})
 var port = process.env.PORT || 3000;
 app.listen(port);
 console.log('Server running on port %d');
