70f7621ac956c8c29721ef0712b84e7a63c7c716
Dale Fenton <fentondale@gmail.com>
2016-04-10 20:43:50 -0400

    added yelp api proxy

--- app/node/app.js
+++ app/node/app.js
@@ -1,29 +1,60 @@
 var express = require('express');
 var request = require('request');
+var Yelp = require('yelp');
 var app = express();
 var cors = require('cors');
 
 app.use(cors()); //allows overriding cross origin policy (use npm install if needed)
 
 
-app.get('/api', function(req, res){ // listens for request on /api route
-  var code = req.query.token;
-  var client_id = process.env.CLIENT_ID;
-  var client_secret = process.env.CLIENT_SECRET;
-  request('https://github.com/login/oauth/access_token?client_id=' + client_id
-            + '&client_secret=' + client_secret
-            + '&code=' + code,
+app.get('/api/gasfeed', function(req, res){ // listens for request on /api route
+  var key = process.env.GASFEED_API || 'o09ii81rr5';
+  var endpoint, options;
+  if(req.query.options){
+    options = JSON.parse(req.query.options);
+  }
+  endpoint = 'http://api.mygasfeed.com/' + req.query.endpoint +  key + '.json';
+  request( endpoint,
     function (error, response, body) { // api url
       if (!error && response.statusCode === 200) {
         res.send(body); // if no errors, send the body of data back to front end
       }
+      if(error){
+        res.send(error, response, body);
+      }
    });
 });
 
-app.get('/client-id', function(req, res){
-  var client_id = process.env.CLIENT_ID;
-  res.send(client_id);
+app.get('/api/yelp', function(req, res){
+  var ckey = process.env.YELP_CONSUMER_KEY || 'dl6yJ0IjtSbF2i4VcQAyJg';
+  var csecret = process.env.YELP_CONSUMER_SECRET || 'LyKDEwmpIzq7aCbqChMtvDsh3TE';
+  var token = process.env.YELP_TOKEN || 'PwtCgTe9HjOvXnmtnso-pRr8impN_Qxu';
+  var token_secret = process.env.YELP_TOKEN_SECRET || 'tXy1U-oNYvwUcfS--JyLNfo_Rg8';
+
+  var yelp = new Yelp({
+    consumer_key: ckey,
+    consumer_secret: csecret,
+    token: token,
+    token_secret: token_secret,
+  });
+
+  // var term = req.query.term;
+  // var location = req.query.location;
+  // var cll = req.query.cll;
+  // var radius = req.query.radius;
+
+  yelp.search({
+    term: "german food",
+    location: "Hayes",
+    cll: "37.77493,-122.419415",
+    radius_filter: "10"
+  }).then(function(data){
+    res.send(data);
+  }).catch(function(err){
+    console.error(err);
+  });
 });
 var port = process.env.PORT || 3000;
 app.listen(port);
-console.log('Server running on port %d');
+
+// console.log('server running on port: ', port);
